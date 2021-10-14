b2537f6e4b0e84df8615baeb1da67ca7b919dcc5
Shinrobin <shinrobin97@naver.com>
2021-10-15 03:52:06 +0900

    기본틀

--- project.py
+++ project.py
@@ -1,11 +1,28 @@
 from pico2d import *
 import random
+# Game object class here
 
+class Grass:
+    def __init__(self): # 생성자
+        self.image = load_image('grass.png')
 
+    def draw(self):
+        self.image.draw(400, 30)
 
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+class Boy:
+    def __init__(self):
+        self.image = load_image('run_animation.png')
+        self.x = random.randint(100, 700)
+        self.y = 90
+        self.frame = random.randint(0, 7)
 
-# 이 함수는 분석할 필요 없음. 기존 코드.
+    def update(self): # 소년의 행위 구현
+        self.x += 5 # 속성값을 바꿈으로써, 행위(오른쪽으로 이동) 구현
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.clip_draw(self.frame* 100, 0, 100, 100, self.x, self.y)
+
 def handle_events():
     global running
     events = get_events()
@@ -14,95 +31,40 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-    pass
 
-# 이 함수는 10개의 랜덤 위치의 화살표를 화면에 그려줌. 분석 필요 없음.
-def draw_all_arrows():
-    for i, p in enumerate(target_points):
-        arrow.draw(p[0], p[1])
-        pico2d.debug_font.draw(p[0], p[1], str(i), (255,0,0))
+# initialization code
 
+open_canvas()
 
+grass = Grass()
+# boy = Boy()
 
-# 이 이후의 코드에 대한 분석하면 됨.
-
-
-def update_character():
-    global cx, cy
-    global running_right
-    global p1, p2, p3, p4
-    global t
-    global prev_cx
-
-    cx = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-    cy = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-
-    # 다음 코드가 하는 일은?
-    t += 0.001
-    if t >= 1.0:
-        p1, p2, p3, p4 = get_next_four_points()
-        t = 0
-
-
-    # 다음 코드의 목적은?
-    running_right = cx > prev_cx
-    prev_cx = cx
-
-
-# 아래 코드는 어떤 식으로 4개의 포인트를 가져오는가?
-def get_next_four_points():
-    global cur_index
-    start = cur_index % num_points
-    end = start + 4
-    points = extended_target_points[start:end]
-    cur_index += 1
-    return points
-
-
-open_canvas(KPU_WIDTH, KPU_HEIGHT)
-
-# prepare images
-kpu_ground = load_image('KPU_GROUND.png')
-character = load_image('animation_sheet.png')
-arrow = load_image('hand_arrow.png')
+# team
+team = [ Boy() for i in range(11) ]
 
 running = True
-running_right = True
-cx, cy = KPU_WIDTH // 2, KPU_HEIGHT // 2
-prev_cx = cx
-frame = 0
-hide_cursor()
 
-num_points = 10
-target_points = []
-for i in range(num_points):
-    target_points.append((random.randint(100, KPU_WIDTH-100), random.randint(100, KPU_HEIGHT-100)))
 
-# 아래의 코드에서, target_points[:3]을 더해서, extended_target_points를 만든 이유는?
-extended_target_points = target_points + target_points[:3]
-cur_index = -1
 
-t = 0
-p1, p2, p3, p4 = get_next_four_points()
+# game main loop code
 
 while running:
 
+    handle_events() # 키 입력 받아들이는 처리..
+
+    # Game logic
+    for boy in team:
+        boy.update() # 소년의 상호작용
+
+    # Game drawing
     clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    draw_all_arrows()
-    if running_right:
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
-    else:
-        character.clip_draw(frame * 100, 100 * 0, 100, 100, cx, cy)
+    grass.draw()
+    for boy in team:
+        boy.draw()
+
     update_canvas()
 
-    frame = (frame + 1) % 8
-    update_character()
+    delay(0.05)
 
-    handle_events()
-
-close_canvas()
-
-
-
+# finalization code
 
