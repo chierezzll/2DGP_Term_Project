be500e27bcb8dc65427973ae05d583179a904657
Shinrobin <shinrobin97@naver.com>
2021-10-17 01:25:47 +0900

    마리오 생성

--- project.py
+++ project.py
@@ -23,24 +23,52 @@ class Boy:
     def draw(self):
         self.image.clip_draw(self.frame* 100, 0, 100, 100, self.x, self.y)
 
+class Mario:
+
+    def __init__(self):
+        self.image = load_image('mario_sheet.png')
+        self.x = 300
+        self.y = 400
+        self.dir = 0
+        self.frame = 350
+        self.width = 40
+
+    def update(self):
+        self.frame = (self.frame + 1 ) % 4
+        self.x += 5 * self.dir
+
+
+    def draw(self):
+        self.image.clip_draw(350 + self.frame * 45 , 90, 40, 90, self.x, self.y)          # 350, 400, 450, 500
+
 def handle_events():
     global running
+    global mario
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                mario.dir += 1
+            elif event.key ==SDLK_LEFT:
+                mario.dir -= 1
+
+        elif event.type == SDL_KEYUP:
+            if event.key ==SDLK_RIGHT:
+                mario.dir -= 1
+            elif event.key ==SDLK_LEFT:
+                mario.dir += 1
+
 
 # initialization code
 
 open_canvas(1920, 1080)
 
 grass = Grass()
-# boy = Boy()
-
-# team
-
+mario = Mario()
 running = True
 
 
@@ -52,10 +80,12 @@ while running:
     handle_events() # 키 입력 받아들이는 처리..
 
     # Game logic
+    mario.update()
 
     # Game drawing
     clear_canvas()
     grass.draw()
+    mario.draw()
 
     update_canvas()
 
